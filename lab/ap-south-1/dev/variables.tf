1c719c0519aaf7c0d5f10a289ad6c3e84762df46
a.mohankumar <a.mohankumar@rishabhsoft.com>
2018-12-20 09:15:27 +0000

    created Subnet for public and private, Internet gateway  and routetable

--- lab/ap-south-1/dev/variables.tf
+++ lab/ap-south-1/dev/variables.tf
@@ -13,3 +13,12 @@ variable "instance_tanancy" {
   default = "default"
 }
 
+variable "public_subnet_cidr" {
+ description = "CIDR for the public subnet"
+ default = "10.1.1.0/24"
+}
+
+variable "private_subnet_cidr" {
+ description = "CIDR for the private subnet"
+ default = "10.1.2.0/24"
+}
--- lab/ap-south-1/dev/vpc.tf
+++ lab/ap-south-1/dev/vpc.tf
@@ -11,3 +11,54 @@ resource "aws_vpc" "main" {
     application = "vpc"
   }
 }
+
+#public subnet
+resource "aws_subnet" "public subnet" {
+vpc_id = "$(vpc.default.id}"
+cidr_block = "${var.public_subnet_cidr}"
+availability_zone = "${var.environment}"
+
+tags {
+  Name = "public subnet"
+ }
+}
+
+
+#private subnet
+resource "aws_subnet" "private subnet" {
+vpc_id = "$(vpc.default.id}"
+cidr_block = "${var.private_subnet_cidr}"
+availability_zone = "${var.environment}"
+
+tags {
+  Name = "private subnet"
+ }
+}
+
+#Internet Accessgateway
+resource "internet_gateway" "gw" {
+  vpc_id = "${vpc.default.id}"
+
+  tags {
+    Name = "VPC IGW"
+  }
+}
+
+#Routetable
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${vpc.default.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${internet_gateway.gw.id}"
+  }
+
+  tags 
+    Name = "Public Subnet RT"
+  }
+
+# Assign the route table to the public Subnet
+resource "route_table_association" "web-public-rt" {
+  subnet_id = "${subnet.public-subnet.id}"
+  route_table_id = "${route_table.web-public-rt.id}"
+}
